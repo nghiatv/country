ec2f584696ea1e0f34b20d93d03743e5d607cea1
nghiatv <nghiait0111@gmail.com>
2019-06-01 14:58:32 +0700

    Updates

---
+++ precache-manifest.e6b5597f35094f8501e909fe4c936bea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "758987b1af554035342efe2caccf224a",
+    "url": "/country/index.html"
+  },
+  {
+    "revision": "8681e015ddd13d806b32",
+    "url": "/country/static/css/main.a468f6bc.chunk.css"
+  },
+  {
+    "revision": "acbc8a41eabf98a26c36",
+    "url": "/country/static/js/2.e0434940.chunk.js"
+  },
+  {
+    "revision": "8681e015ddd13d806b32",
+    "url": "/country/static/js/main.f508f8f4.chunk.js"
+  },
+  {
+    "revision": "be7ed9fc8b1f8ca1fd63",
+    "url": "/country/static/js/runtime~main.3bd37249.js"
+  }
+]);
