31abf82f175aec6d8683502d035cf7203ced7531
nghiatv <nghiait0111@gmail.com>
2019-06-01 15:16:33 +0700

    fix gh-pages deploy

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,18 @@ import { Navbar, Nav } from "react-bootstrap";
 import { Country } from "./screens/Country";
 
 const App: React.FC = () => {
+  const basename = process.env.NODE_ENV === "production" ? "/country/" : "/";
   return (
-    <Router>
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="/">React Boostrap Demo</Navbar.Brand>
+    <Router basename={basename}>
+      <Navbar bg="dark" expand="lg">
+        <Navbar.Brand className="text-white" href={basename}>
+          React Boostrap Demo
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
             <Nav.Item>
-              <Link className="nav-link" to="/">
+              <Link className="nav-link text-white" to="/">
                 Home
               </Link>
             </Nav.Item>
